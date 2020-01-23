f111dc6fa89d313e87929f4235c42609922d7094
chandramfp <chandrashekhar.kumar@successive.tech>
2020-01-23 18:17:49 +0530

    equilateral program

--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,14 +1,16 @@
 function equilateral(n)
 {
-    if(n<2 || n>10)
+    
+    if(n<2 && n>10)
      return "please enter value between 2 to 10";
-     for(let i = 0; i < n; i++)
+     
+     for(let i = 1; i <= n; i++)
      {
          let p=''
-         for(let j = n; j>=i;j--)
+         for(let j = i; j<=n;j++)
           //console.log(" ");
           p+=" "
-        for( let k = 0;k<i;k++)
+        for( let k = 1;k<=i;k++)
          //console.log("*");
          p+="*"+" "
          console.log(p);
