14f88fb46e5472aa9e8eaa1b633141789e67d37d
chandramfp <chandrashekhar.kumar@successive.tech>
2020-01-24 20:52:48 +0530

    Second task

---
+++ extra/utils/permissions.js
@@ -0,0 +1,24 @@
+
+const permissions =
+{
+    'getUsers': {
+    all: ['head-trainer'],
+    read : ['trainee', 'trainer'],
+    write : ['trainer'],
+    delete: [],
+}
+
+};
+
+function hasPermission(moduleName, role, permissionType)
+{
+    let data = permissions[moduleName];
+    let x = data[permissionType];
+    return x.some(element => { if (element === role)
+    return true;
+    else
+    return false});
+}
+//console.log(hasPermission("getUsers","tra","read"));
+let temp = (hasPermission("getUsers","trainer","read"));
+console.log(temp)
