31a707f51e3d9d80fdcb5965a54f286aaf66d3b4
chandramfp <chandrashekhar.kumar@successive.tech>
2020-01-24 10:00:12 +0530

    solved the issues

--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,22 +1,24 @@
 function equilateral(n)
 {
     
-    if(n<2 && n>10)
+    if(n < 2 || n > 10)
      return "please enter value between 2 to 10";
      
-     for(let i = 1; i <= n; i++)
-     {
-         let p=''
-         for(let j = i; j<=n;j++)
-          //console.log(" ");
-          p+=" "
-        for( let k = 1;k<=i;k++)
-         //console.log("*");
-         p+="*"+" "
-         console.log(p);
-     }
+    for(let i = 1; i <= n; i++)
+    {
+        let p = '';
+
+        for(let j = i; j <= n; j++)
+          p += " ";
+
+        for( let k = 1;k <= i; k++)
+          p += "*" + " ";
+
+        console.log(p);
+    }
 }
 
-let t=Number(process.argv[2]);
+let t = Number(process.argv[2]);
+
 equilateral(t);
 //console.log(process.argv);
