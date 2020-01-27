e3f97a4d7c115f042a9eeedef70346990ad023ce
chandramfp <chandrashekhar.kumar@successive.tech>
2020-01-27 09:54:32 +0530

    program with changes

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -2,23 +2,24 @@
 const permissions =
 {
     'getUsers': {
-    all: ['head-trainer'],
-    read : ['trainee', 'trainer'],
-    write : ['trainer'],
-    delete: [],
-}
+        all: ['head-trainer'],
+        read : ['trainee', 'trainer'],
+        write : ['trainer'],
+        delete: [],
+    }
 
 };
 
 function hasPermission(moduleName, role, permissionType)
 {
     let data = permissions[moduleName];
-    let x = data[permissionType];
-    return x.some(element => { if (element === role)
+    let temp = data[permissionType];
+    return temp.some(element => { if (element === role)
     return true;
     else
-    return false});
+    return false 
+   } );
 }
 //console.log(hasPermission("getUsers","tra","read"));
-let temp = (hasPermission("getUsers","trainer","read"));
-console.log(temp)
+let result = (hasPermission("getUsers","trainer","read"));
+console.log(result)
