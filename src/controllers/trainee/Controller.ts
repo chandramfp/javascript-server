e0b760b9e5a74d66f866799d67352cafdf1df3d1
chandramfp <chandrashekhar.kumar@successive.tech>
2020-01-31 15:59:10 +0530

    Controller folder

---
+++ src/controllers/trainee/Controller.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from 'express';
+class Controller {
+    static instance: Controller;
+    static getInstance = () => {
+        if (Controller.instance) {
+            return Controller.instance;
+        }
+        Controller.instance = new Controller();
+        return Controller.instance;
+    }
+
+    create = (req: Request, res: Response) => {
+        console.log('::::::: INSIDE CREATE TRAINEE :::::::');
+        res.send({
+            status: 'Ok',
+            message: 'Trainee Added Successfully',
+            data: {
+                Id: '1001',
+                name: 'Jhon',
+                address: 'Noida'
+            }
+        });
+    }
+
+    list = (req: Request, res: Response) => {
+        console.log('::::::: INSIDE TRAINEE LIST :::::::');
+        res.send({
+            status: 'Ok',
+            message: 'List Of Trainee',
+            data: [{
+                Id: '1001',
+                name: 'Jhon',
+                address: 'Noida'
+            },
+            {
+                Id: '1002',
+                name: 'Alex',
+                address: 'Pune'
+            },
+            {
+                Id: '1003',
+                name: 'Alexander',
+                address: 'Delhi'
+            }
+            ],
+
+        });
+
+    }
+    delete = (req: Request, res: Response) => {
+        console.log('::::::: INSIDE DELETE RECORD :::::::');
+        res.send({
+            status: 'Ok',
+            message: 'Record Deleted Successfully',
+            data: {
+                Id: '1001',
+                name: 'Jhon',
+                address: 'Noida'
+            }
+        });
+    }
+    update = (req: Request, res: Response) => {
+        console.log('::::::: INSIDE UPDATE RECORD :::::::');
+        res.send({
+            status: 'Ok',
+            message: 'Record Updated Successfully',
+            data: {
+                Id: '1004',
+                name: 'Mark',
+                address: 'Mumbai'
+            }
+        });
+    }
+
+
+
+
+
+}
+
+export default Controller.getInstance();
