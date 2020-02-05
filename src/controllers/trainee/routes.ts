81a70c4e9985d6f8aabea2caf605898c4781ac55
chandramfp <chandrashekhar.kumar@successive.tech>
2020-02-05 13:12:32 +0530

    updated routes

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import  Controller from './Controller';
+
+import validationHandler from '../../libs/validationHandler';
+import validation from './validation';
+
 const traineeRouter = Router();
 traineeRouter.route('/trainee')
-.get(Controller.get)
-.post(Controller.create)
-.put(Controller.update)
-.delete(Controller.delete);
+.get(validationHandler(validation.get),Controller.get)
+.post(validationHandler(validation.create),Controller.create)
+.put(validationHandler(validation.update),Controller.update)
+.delete(validationHandler(validation.delete),Controller.delete);
 
 
 export default traineeRouter;
