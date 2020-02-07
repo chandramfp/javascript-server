eb980e1d2c210da8010236850a99912c03b444c7
chandramfp <chandrashekhar.kumar@successive.tech>
2020-02-07 13:07:57 +0530

    Fixes related to PR

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -7,12 +7,12 @@ import validation from './validation';
 const traineeRouter = Router();
 //console.log('---------Test');
 traineeRouter.route('/trainee')
-.get(validationHandler(validation.get),Controller.get)
-//  .get(authmiddleware('getUsers','read'),validationHandler(validation.get),Controller.get)
-// .post(authmiddleware('getUsers','write'),validationHandler(validation.create),Controller.create)
-.post(validationHandler(validation.create),Controller.create)
-// .put(authmiddleware('getUsers','read'),validationHandler(validation.update),Controller.update)
-.put(validationHandler(validation.update),Controller.update)
+// .get(validationHandler(validation.get),Controller.get)
+ .get(authmiddleware('getUsers','read'),validationHandler(validation.get),Controller.get)
+.post(authmiddleware('getUsers','write'),validationHandler(validation.create),Controller.create)
+// .post(validationHandler(validation.create),Controller.create)
+.put(authmiddleware('getUsers','read'),validationHandler(validation.update),Controller.update)
+// .put(validationHandler(validation.update),Controller.update)
 
 // .delete(authmiddleware('getUsers','read'),validationHandler(validation.delete),Controller.delete);
 .delete(authmiddleware(validationHandler(validation.delete),Controller.delete));
