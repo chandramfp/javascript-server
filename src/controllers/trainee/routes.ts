a414d420d42c22d416a5da7c07df995d3440e088
chandramfp <chandrashekhar.kumar@successive.tech>
2020-02-07 12:55:34 +0530

    feat feature 39524 and 40009

--- src/controllers/trainee/routes.ts
+++ src/controllers/trainee/routes.ts
@@ -8,14 +8,14 @@ const traineeRouter = Router();
 //console.log('---------Test');
 traineeRouter.route('/trainee')
 .get(validationHandler(validation.get),Controller.get)
-// .get(authmiddleware('getUsers','read'),validationHandler(validation.get),Controller.get)
-//.post(authmiddleware('getUsers','write'),validationHandler(validation.create),Controller.create)
+//  .get(authmiddleware('getUsers','read'),validationHandler(validation.get),Controller.get)
+// .post(authmiddleware('getUsers','write'),validationHandler(validation.create),Controller.create)
 .post(validationHandler(validation.create),Controller.create)
-//.put(authmiddleware('getUsers','read'),validationHandler(validation.update),Controller.update)
+// .put(authmiddleware('getUsers','read'),validationHandler(validation.update),Controller.update)
 .put(validationHandler(validation.update),Controller.update)
 
-//.delete(authmiddleware('getUsers','read'),validationHandler(validation.delete),Controller.delete);
-//.delete(authmiddleware(validationHandler(validation.delete),Controller.delete);
+// .delete(authmiddleware('getUsers','read'),validationHandler(validation.delete),Controller.delete);
+.delete(authmiddleware(validationHandler(validation.delete),Controller.delete));
 
 traineeRouter.delete('/trainee/:id',validationHandler(validation.delete),Controller.delete)
 
